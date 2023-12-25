5a84ba653058536f94c32c615ea1f3c6048e9036
Kirill Pavlovskii <kirill-pavlovskij@rambler.ru>
2023-12-25 22:26:51 +0300

    feat: maximumPopulation O(NxK)

--- src/prefixSum/populationYear.js
+++ src/prefixSum/populationYear.js
@@ -4,11 +4,9 @@
  * @param {number[][]} logs
  * @return {number}
  */
-var maximumPopulation = function(logs) {
-  // let maxYear = findMaxlogs(logs);
-  // let years = Array.from({ length: maxYear }).map(() => 0);
+var maximumPopulationBF = function(logs) {
   let maxYear = Math.max(...logs.map(log => log[1]));
-  let years = Array(maxYear).fill(0)
+  let years = Array(maxYear).fill(0);
   for(let i = 0; i < logs.length; i++) {
     const [birth, death] = logs[i];
     for (let j = birth; j < death; j++) {
@@ -16,11 +14,6 @@ var maximumPopulation = function(logs) {
     }
   }
   let maxYearPopulation = Math.max(...years);
-  // for (let i = 0; i < years.length; i++) {
-  //   if (years[i] > maxYearPopulation) {
-  //     maxYearPopulation = years[i];
-  //   }
-  // }
   return years.indexOf(maxYearPopulation);
 };
 
@@ -34,12 +27,57 @@ var maximumPopulation = function(logs) {
 // console.log(maximumPopulation([[1950,1961],[1960,1971],[1970,1981]])); // 2
 
 
+// console.log(maximumPopulationBF([
+//   [5, 10],
+//   [2, 7],
+//   [4, 15],
+//   [1, 6],
+//   [9, 12],
+//   [8, 20],
+//   [16, 23],
+// ]));
+
+/**
+ * @param {number[][]} logs
+ * @return {number}
+ */
+var maximumPopulation = function(logs) {
+  let max = Math.max(...logs.map(log => log[1]));
+  let min = logs.reduce((acc, log) => Math.min(acc, log[0]), Infinity);
+  let diffs = Array(max - min + 1).fill(0);
+  for(const [b, d] of logs) {
+    diffs[b - min]++;
+    diffs[d - min]--;
+  }
+  let prexifSum = 0;
+  let maxResult = 0;
+  let index = 0;
+  let maxIndex = 0;
+  for(const diff of diffs) {
+    prexifSum += diff;
+    if (prexifSum > maxResult) {
+      maxResult = prexifSum;
+      maxIndex = index;
+    }
+    index++;
+  }
+  return maxIndex + min;
+};
+
+// [                  !~
+//   1, 1,  0, 0,  1,  1, -1,
+//   1, 0,  0, 0, -1,  0, -1,
+//   0, 0, -1, 0,  0, -1
+// ]
+
 console.log(maximumPopulation([
-  [5, 10],
-  [2, 7],
+  [5, 10], //
+  [12, 17],
   [4, 15],
-  [1, 6],
+  [11, 16],
   [9, 12],
   [8, 20],
   [16, 23],
-]))
+]));
+
+/// https://magma.com/d/0FOKS5vSDu
